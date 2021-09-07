62ca2e1c281bc0ed34cbd6823823b637aa32c2f0
joao rafael fagundes menezes <menezes.jrafael@gmail.com>
2021-09-07 19:01:24 -0300

    Primeiro COmmit

---
+++ Aula02/index.js
@@ -0,0 +1,11 @@
+var modulo = require('./modulo');
+var n1 = 5;
+var n2 = 8;
+
+
+if(modulo.somar(n1, n2) === 14){
+    console.log('O resultado da soma é : ' + modulo.somar(n1, n2));
+}
+else{
+    console.log('Não foi possível realizar o processo...');
+}
